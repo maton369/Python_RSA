1d1dbe4dec4f7e5e709e28aa43f9c8493c423c6b
maton369 <ysk901085@gmail.com>
2025-06-20 09:21:36 +0900

    Add: 秘密鍵から公開鍵を生成

--- exec.sh
+++ exec.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 # openssl genrsa 2048 > secret_2048.key | tee output.log
-openssl rsa -text < secret_2048.key | tee output.log
+# openssl rsa -text < secret_2048.key | tee output.log
+
+openssl rsa -pubout < secret_2048.key > public_2048.key
+cat public_2048.key | tee output.log
