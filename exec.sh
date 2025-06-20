817b8c6c353cd0795be318feedf2ab962fe860ff
maton369 <ysk901085@gmail.com>
2025-06-20 09:30:17 +0900

    Add: OpenSSLで復号

--- exec.sh
+++ exec.sh
@@ -5,7 +5,8 @@
 
 # openssl rsa -pubout < secret_2048.key > public_2048.key
 # cat public_2048.key | tee output.log
-echo -n "112233445566778899aabbccddeeff00" \
-| xxd -pr -r \
-| openssl pkeyutl -encrypt -pubin -inkey public_2048.key \
-| hexdump -Cv | tee output.log
+# echo -n "112233445566778899aabbccddeeff00" \
+# | xxd -pr -r \
+# | openssl pkeyutl -encrypt -pubin -inkey public_2048.key \
+# | hexdump -Cv | tee output.log
+echo -n "862c975ebed9ba82c9b207a6a08e20c0" | xxd -pr -r | openssl rsautl -decrypt -raw -inkey secret_2048.key | hexdump -Cv | tee output.log
