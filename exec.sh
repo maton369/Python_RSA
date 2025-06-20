e5c4701b6e0220eb26cd981ce190e6b06f339466
maton369 <ysk901085@gmail.com>
2025-06-20 09:27:04 +0900

    Add: OpenSSLで暗号化

--- exec.sh
+++ exec.sh
@@ -3,5 +3,9 @@
 # openssl genrsa 2048 > secret_2048.key | tee output.log
 # openssl rsa -text < secret_2048.key | tee output.log
 
-openssl rsa -pubout < secret_2048.key > public_2048.key
-cat public_2048.key | tee output.log
+# openssl rsa -pubout < secret_2048.key > public_2048.key
+# cat public_2048.key | tee output.log
+echo -n "112233445566778899aabbccddeeff00" \
+| xxd -pr -r \
+| openssl pkeyutl -encrypt -pubin -inkey public_2048.key \
+| hexdump -Cv | tee output.log
