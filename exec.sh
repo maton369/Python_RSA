99784d36b1947885d2c568c54c17a73a97b6101f
maton369 <ysk901085@gmail.com>
2025-06-20 09:49:11 +0900

    Add: OpenSSLで署名

--- exec.sh
+++ exec.sh
@@ -9,4 +9,10 @@
 # | xxd -pr -r \
 # | openssl pkeyutl -encrypt -pubin -inkey public_2048.key \
 # | hexdump -Cv | tee output.log
-echo -n "862c975ebed9ba82c9b207a6a08e20c0" | xxd -pr -r | openssl rsautl -decrypt -raw -inkey secret_2048.key | hexdump -Cv | tee output.log
+# echo -n "862c975ebed9ba82c9b207a6a08e20c0" | xxd -pr -r | openssl rsautl -decrypt -raw -inkey secret_2048.key | hexdump -Cv | tee output.log
+# 平文（16バイトの16進）をバイナリに変換し、RSA秘密鍵で "署名（rawモード）"
+echo -n "112233445566778899aabbccddeeff00" \
+| xxd -pr -r \
+| openssl pkeyutl -sign -inkey secret_2048.key \
+-pkeyopt rsa_padding_mode:pkcs1 \
+| hexdump -Cv | tee output.log
